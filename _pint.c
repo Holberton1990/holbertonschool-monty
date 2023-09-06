c8cae9cf39dedd11b1d497adfbb943a2ec8ba146
Holberton1990 <ersida9019@gmail.com>
2023-09-06 03:19:44 -0700

    C - Stacks, Queues - LIFO, FIFO

--- _pint.c
+++ _pint.c
@@ -11,7 +11,7 @@ void _pint(stack_t **top, unsigned int line_number)
 
 	if (*top == NULL)
 {
-    dprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+    dprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
     exit(EXIT_FAILURE);
 }
 
