02a2fdaa0fc38c2e25057fe63c8b5b9ccd3d88ce
Holberton1990 <ersida9019@gmail.com>
2023-09-06 04:19:19 -0700

    C - Stacks, Queues - LIFO, FIFO

---
+++ _nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - doesn't do anything
+ * @top: double pointer to the top of the stack
+ * @line_number: line number of the file
+ */
+void nop(stack_t **top, unsigned int line_number)
+{
+    (void)top;
+    (void)line_number;
+    /* This function does nothing */
+}
+
