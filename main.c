ebed121993d574ab29aee24c86c563a12c0d08a0
Holberton1990 <ersida9019@gmail.com>
2023-09-07 03:07:16 -0700

    stack

--- main.c
+++ main.c
@@ -21,14 +21,14 @@ int main(int argc, char *argv[])
 
     if (argc != 2)
     {
-        dprintf(STDOUT_FILENO, "USAGE: monty file\n");
+        fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
 
     fs = fopen(argv[1], "r");
     if (fs == NULL)
     {
-        dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
 
     fclose(fs);
     free(lineptr);
-
+    free_stack(&stack); 
     exit(EXIT_SUCCESS);
 }
 
