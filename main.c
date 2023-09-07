1bdeb49d5b7423bed36710c61188a1283f9fb497
Holberton1990 <ersida9019@gmail.com>
2023-09-07 05:38:42 -0700

    monty

--- main.c
+++ main.c
@@ -45,15 +45,11 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
 
-        // Execute the opcode function
-        op_func(&stack, line_number);
     }
 
-    // Clean up and close the file
     free_stack(&stack);
     fclose(monty_file);
     free(line);
 
     return (EXIT_SUCCESS);
 }
-
