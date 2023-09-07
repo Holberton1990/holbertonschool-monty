c94e2fc238ae645a88cad9a111c1faca55f41eaa
Holberton1990 <ersida9019@gmail.com>
2023-09-07 06:10:09 -0700

    task 0

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        fprintf(stderr, "USAGE: monty file\n");  // Updated error message format
         exit(EXIT_FAILURE);
     }
 
