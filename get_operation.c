a8ac2e036d6c8c371f3cbddaf59a1040633891cd
Holberton1990 <ersida9019@gmail.com>
2023-09-08 12:45:27 -0700

    monty

--- get_operation.c
+++ get_operation.c
@@ -1,53 +1,60 @@
 #include "monty.h"
 
 stack_t *stack = NULL;
+
 /**
  * free_stack - Frees a stack
  * @stack: Pointer to the head of the stack
  */
 void free_stack(stack_t **stack)
 {
-	stack_t *current, *temp;
-	if (stack == NULL || *stack == NULL)
-		return;
-	current = *stack;
-	while (current != NULL)
-	{
-		temp = current;
-		current = current->next;
-		free(temp);
-	}
+    stack_t *current, *temp;
+    if (stack == NULL || *stack == NULL)
+        return;
+
+    current = *stack;
+    while (current != NULL)
+    {
+        temp = current;
+        current = current->next;
+        free(temp);
+    }
 }
+
 /**
  * execute_instruction - Executes a Monty bytecode instruction
  * @opcode: The opcode to execute
  * @stack: Pointer to the head of the stack
  * @line_number: The current line number in the Monty file
  */
-void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number){
-	instruction_t instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{NULL, NULL}
-	};
-	int i = 0;
+void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number)
+{
+    instruction_t instructions[] = {
+        {"push", push},
+        {"pall", pall},
+        {"pint", pint},
+        {"pop", pop},
+        {"swap", swap},
+        {"add", add},
+        {"nop", nop},
+        {NULL, NULL}
+    };
+    int i = 0;
 
-	while (instructions[i].opcode != NULL){
-		if (strcmp(opcode, instructions[i].opcode) == 0)
-		{
-			instructions[i].f(stack, line_number);
-			return;
-		}
-		i++;
-	}
-	/* If the opcode is not recognized, print an error message. */
-	monty_error("unknown instruction", line_number, opcode);
+    while (instructions[i].opcode != NULL)
+    {
+        if (strcmp(opcode, instructions[i].opcode) == 0)
+        {
+            instructions[i].f(stack, line_number);
+            return;
+        }
+        i++;
+    }
+
+    /* If the opcode is not recognized, print an error message. */
+    monty_error("unknown instruction", line_number, opcode);
 }
+
 /**
  * monty_error - Prints an error message and exits the program
  * @msg: The error message to print
@@ -56,8 +63,8 @@ void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number
  */
 void monty_error(char *msg, unsigned int line_number, char *opcode)
 {
-	fprintf(stderr, "L%u: %s %s\n", line_number, msg, opcode);
-	free_stack(&stack);
-	exit(EXIT_FAILURE);
+    fprintf(stderr, "L%u: %s %s\n", line_number, msg, opcode);
+    free_stack(stack); // Remove the '&' before 'stack'
+    exit(EXIT_FAILURE);
 }
 
